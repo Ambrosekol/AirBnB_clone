d0c5f33ef54fb5b8775a7c2c64cd4483882917d7
danjor667 <jordannguepi@gmail.com>
2023-08-12 14:19:30 -0700

    new version

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,35 +16,43 @@ class FileStorage():
         """
         returns a dictionary of
         all obj. class.id as key
-        and obj as value
+        and obj as
         """
-        from models.base_model import BaseModel
-        my_dict = {}
-        for k, v in FileStorage.__objects.items():
-            my_dict[k] = eval(k.split(".")[0])(**v)
-        return my_dict
+        return FileStorage.__objects
 
     def save(self):
         """
         adds a new obj to the storage
         """
+        my_dict = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
         with open(FileStorage.__file_path, "w") as f:
-            json.dump(FileStorage.__objects, f)
+            json.dump(my_dict, f)
 
     def new(self, obj):
         """
-        add a new obj dictionary representation
+        add a new obj dictionary
         """
         class_name = obj.__class__.__name__
-        FileStorage.__objects[class_name + "." + obj.id] = obj.to_dict()
+        FileStorage.__objects[class_name + "." + obj.id] = obj
 
     def reload(self):
         """
         reloads all the objs saved in file
         """
-        #print("called")
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.amenity import Amenity
+        from models.city import City
+        from models.state import State
+        from models.review import Review
+        myclasses = {"BaseModel": BaseModel, "User": User, "Place": Place,
+                     "Amenity": Amenity, "City": City, "State": State,
+                     "Review": Review}
         try:
             with open(FileStorage.__file_path, "r") as f:
-                FileStorage.__objects = json.load(f)
+                my_dict = json.load(f)
+                for k, v in my_dict.items():
+                    FileStorage.__objects[k] = myclasses[v["__class__"]](**v)
         except Exception:
             pass
